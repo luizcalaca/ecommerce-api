b671525eee188fd301659a96a346d6a7414052aa
Luiz Calaça <luizcalaca@gmail.com>
2022-08-28 21:55:47 -0300

    test: TDD - creating an order - adding User

--- tests/bdd/order.create.spec.ts
+++ tests/bdd/order.create.spec.ts
@@ -3,7 +3,13 @@ import chaiAsPromised from 'chai-as-promised';
 import * as chai from 'chai'
 import sinon from 'sinon'
 
+class User {
+    name: string
+    email: string
+}
+
 class Order {
+    user: User
     quantity: number
 }
 
@@ -13,8 +19,15 @@ const expect = chai.expect
 
 describe('BDD - Creating an order', () => {
     it('BDD - Should create and order', () => {
+
+        const user: User = {
+            name: "John McBurguer",
+            email: "johnburger@email.com"
+        }
+
         const OrderMock: Order = {
-            quantity: 0
+            quantity: 0,
+            user: user
         }
     })
 })
