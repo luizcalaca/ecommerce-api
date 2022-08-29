07420eb1ab4de715852974d5887160fcd4a080bb
Luiz Calaça <luizcalaca@gmail.com>
2022-08-28 22:19:47 -0300

    test: TDD - creating an order - adding the interface IPersistence and OrderRepository

--- tests/bdd/order.create.spec.ts
+++ tests/bdd/order.create.spec.ts
@@ -2,6 +2,7 @@ import { describe } from "mocha";
 import chaiAsPromised from 'chai-as-promised';
 import * as chai from 'chai'
 import sinon from 'sinon'
+import { stubInterface } from "ts-sinon";
 import { User } from "../../src/domain/entities/User";
 import { Product } from "../../src/domain/entities/Product";
 import { Order } from "../../src/domain/entities/Order";
@@ -9,8 +10,20 @@ import { Order } from "../../src/domain/entities/Order";
 chai.use(chaiAsPromised)
 const expect = chai.expect
 
+interface IPersistence {
+    create(entity: any): Promise<void>
+}
+
+class CreateOrderRepository {
+    constructor(private iPersistence: IPersistence) { }
+
+    public async execute(entity: Order): Promise<void> {
+        this.iPersistence.create(entity)
+    }
+}
+
 describe('BDD - Creating an order', () => {
-    it('BDD - Should create and order', () => {
+    it('BDD - Should create an order', () => {
 
         const userMock: User = {
             name: "John McBurguer",
@@ -27,5 +40,8 @@ describe('BDD - Creating an order', () => {
             user: userMock,
             product: productMock
         }
+
+        const iPersistence = stubInterface<IPersistence>()
+        const orderRepository = new CreateOrderRepository(iPersistence)
     })
 })
